33b3ddbb119cd944537ea31464c0c28c4037c20f
Nils Schimmelmann <nschimme@users.noreply.github.com>
2025-06-14 08:16:59 -0500

    add PWA support

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ const PORT = 4000
 
 const app = express()
 
+app.get('/manifest.webmanifest', (req, res) => {
+  res.sendFile(path.join(__dirname, 'manifest.webmanifest'), {
+    headers: {
+      'Content-Type': 'application/manifest+json'
+    }
+  });
+});
+
+app.get('/sw.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'sw.js'), {
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  });
+});
+
 app.use("/", express.static(__dirname))
 
 app.listen(PORT, function () {
